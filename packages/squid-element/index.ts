13e43199b3775132762fe2947b595b5c8b01a6ff
grandpawang <907415276@qq.com>
2020-12-14 22:12:14 +0800

    feat(components): :construction: 抄一个element组件做website实列

--- packages/squid-element/index.ts
+++ packages/squid-element/index.ts
@@ -1,17 +1,20 @@
 import { version as version_ } from "./version"
 import SCIcon from "@squid-element/icon"
 import Test1 from "@squid-element/test1"
+import Link from "@squid-element/link"
 
 const version = version_
 
 const components = [
   SCIcon,
   Test1,
+  Link,
 ]
 
 export {
   SCIcon,
   Test1,
+  Link,
 }
 
 export default {
