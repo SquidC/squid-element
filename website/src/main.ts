d23effbb82582ecf83fa441dbd774967b4e256a3
莫晋铭 <q502489978@live.com>
2020-12-22 14:41:40 +0800

    chore(global): website引入对应库样式

--- website/src/main.ts
+++ website/src/main.ts
@@ -1,20 +1,23 @@
-import { createApp } from "vue"
-import SCElement from "squid-element"
-import router from "./route"
-import App from "./App.vue"
-import demoBlock from "./components/demo-block.vue"
+import { createApp } from "vue";
+import SCElement from "squid-element";
+import router from "./route";
+import App from "./App.vue";
+import demoBlock from "./components/demo-block.vue";
 
 // 引入 鱿鱼丝组件库样式
-import "@squid-element/theme-chalk/src/index.less"
+import "@squid-element/theme-chalk/src/index.less";
 
 // 引入 鱿鱼丝组件库依赖的element-plus组件库样式
-import "element-plus/lib/theme-chalk/index.css"
+import "element-plus/lib/theme-chalk/index.css";
 
-const app = createApp(App)
+// 引入 highlight库样式
+import "highlight.js/styles/vs2015.css";
 
-app.use(router)
-app.component("DemoBlock", demoBlock)
-app.use(SCElement)
+const app = createApp(App);
 
-app.mount("#app")
-export default app
+app.use(router);
+app.component("DemoBlock", demoBlock);
+app.use(SCElement);
+
+app.mount("#app");
+export default app;
