bc7674814572e603db517f8598da61fa1ca9bdb8
莫晋铭 <q502489978@live.com>
2020-12-29 17:44:21 +0800

    refactor(hooks): 修改hook

--- packages/hooks/src/useRefTemplate.ts
+++ packages/hooks/src/useRefTemplate.ts
@@ -1,9 +1,22 @@
 import { Ref, unref } from "vue";
 
-export function useRefTemplate<T>(refTemplate: Ref<RefTemplate<T>>) {
+
+export function useRefTemplate<T>(refTemplate: Ref<RefTemplate<T>>): T;
+export function useRefTemplate<T>(
+  refTemplate: Ref<RefTemplate<T>>,
+  isNull: boolean
+): RefTemplate<T>;
+export function useRefTemplate<T>(
+  refTemplate: Ref<RefTemplate<T>>,
+  isNull?: boolean,
+): RefTemplate<T> {
   const el = unref(refTemplate);
   if (!el) {
-    throw new Error("找不到元素");
+    if (!isNull) {
+      throw new Error("找不到元素");
+    } else {
+      return el;
+    }
   }
   return el as T;
 }
